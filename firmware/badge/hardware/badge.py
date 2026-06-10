c2bc52ba9e9b1f513d3327fd2cb9426597d6b6e5
Henrique <henrique.grolli@gmail.com>
2026-06-10 20:46:55 +0200

    Added failsafe when setting up the default values for radio config.
    Now the setup function will restore to a default if it expects to find
    a int or float and find something unparseable instead. This prevents
    the application from crashing on startup.

--- firmware/badge/hardware/badge.py
+++ firmware/badge/hardware/badge.py
@@ -30,38 +30,15 @@ class Badge:
         # Reserve controller 0 for the SAO header so it never collides with the keyboard bus.
         self.sao_i2c = I2C(0, scl=board.SAO_SCL, sda=board.SAO_SDA, freq=400000)
 
-        # Helper functions to safely decode and parse config values
-        def get_int(key):
-            val = self.config.get(key)
-            if val is None:
-                raise ValueError("Config " + key + " is empty!")
-            if isinstance(val, bytes):
-                val = val.decode()
-            try:
-                return int(val)
-            except ValueError:
-                raise ValueError("Config " + key + " is not a valid int!")
-
-        def get_float(key):
-            val = self.config.get(key)
-            if val is None:
-                raise ValueError("Config " + key + " is empty!")
-            if isinstance(val, bytes):
-                val = val.decode()
-            try:
-                return float(val)
-            except ValueError:
-                raise ValueError("Config " + key + " is not a valid float!")
-
         board.DEBUG_LED.off()  # Default LED off
 
         # Setup radio
-        tx_power = get_int("radio_tx_power")
-        frequency = get_float("radio_frequency")
-        bandwidth = get_float("radio_bandwidth")
-        spreading_factor = get_int("radio_spreading_factor")
-        coding_rate = get_int("radio_coding_rate")
-        self.send_cooldown_ms = get_int("send_cooldown_ms")
+        tx_power = self.get_int("radio_tx_power")
+        frequency = self.get_float("radio_frequency")
+        bandwidth = self.get_float("radio_bandwidth")
+        spreading_factor = self.get_int("radio_spreading_factor")
+        coding_rate = self.get_int("radio_coding_rate")
+        self.send_cooldown_ms = self.get_int("send_cooldown_ms")
 
         print (
             "Initializing LoRa radio tx power {}, frequency {}, bandwidth {}, SF{}, CR{}."
@@ -91,18 +68,52 @@ class Badge:
         self._setup_default_config_value("nametag", "Your Name Here!")
         self._setup_default_config_value("nametag_show_image", b'false')
         self._setup_default_config_value("nametag_image", b'images/headshots/wrencher.png')
-        self._setup_default_config_value("radio_tx_power", b'9')
-        self._setup_default_config_value("radio_frequency", b'869.525')
-        self._setup_default_config_value("radio_bandwidth", b'250.0')
-        self._setup_default_config_value("radio_spreading_factor", b'7')
-        self._setup_default_config_value("radio_coding_rate", b'5')
-        self._setup_default_config_value("chat_ttl", b'3')
-        self._setup_default_config_value("send_cooldown_ms", b'1')
+        self._setup_default_int("radio_tx_power", b'9')
+        self._setup_default_float("radio_frequency", b'869.525')
+        self._setup_default_float("radio_bandwidth", b'250.0')
+        self._setup_default_int("radio_spreading_factor", b'7')
+        self._setup_default_int("radio_coding_rate", b'5')
+        self._setup_default_int("chat_ttl", b'3')
+        self._setup_default_int("send_cooldown_ms", b'1')
 
     def _setup_default_config_value(self, key, default_value):
         if key not in self.config.db.keys():
             print ("Key {} not found, initialized with {}.".format(key, default_value))
             self.config.set(key, default_value)
+
+    def _setup_default_int(self, key, default_value):
+        self._setup_default_config_value(key, default_value)
+        val = self.config.get(key)
+        if isinstance(val, bytes):
+            val = val.decode()
+        try:
+            int(val)
+        except ValueError:
+            print("Key {} has invalid int {}, reverting to default {}.".format(key, val, default_value))
+            self.config.set(key, default_value)
+
+    def _setup_default_float(self, key, default_value):
+        self._setup_default_config_value(key, default_value)
+        val = self.config.get(key)
+        if isinstance(val, bytes):
+            val = val.decode()
+        try:
+            float(val)
+        except ValueError:
+            print("Key {} has invalid float {}, reverting to default {}.".format(key, val, default_value))
+            self.config.set(key, default_value)
+
+    def get_int(self, key):
+        val = self.config.get(key)
+        if isinstance(val, bytes):
+            val = val.decode()
+        return int(val)
+
+    def get_float(self, key):
+        val = self.config.get(key)
+        if isinstance(val, bytes):
+            val = val.decode()
+        return float(val)
 
     async def run(self):
         print("Running badge task...")
